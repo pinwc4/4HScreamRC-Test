284c820034418496d4ac698482ae1da7c6d300d0
OwenRex <owenrexshell@4hscream.org>
2022-10-04 20:35:23 -0500

    Start of TensorFlow testing

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp
+public class HelloWorld {
+    // This is hello world program
+    /* This is also a comment
+    wow this is super cooooooooooooooooooooooooooooool
+     */
+    // datatype name
+    private int intTeamNember = 14204;
+    public void init() {
+        DcMotor dcmDoesnotexist;
+    }
+}
