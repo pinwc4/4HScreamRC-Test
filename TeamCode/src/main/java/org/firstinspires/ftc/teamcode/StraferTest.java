b2bc2575b3579eec28e23108f997407b61379cd4
FeignedBlock485 <benelven@4hscream.org>
2022-10-15 13:36:02 -0500

    added data logging to code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferTest.java
@@ -15,51 +15,20 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 
 public class StraferTest extends OpMode {
 
-    //private DcMotor dcmCarouselMotor1;
-   // private DcMotor dcmFrontRightMotor;
-  //  private DcMotor dcmBackLeftMotor;
-  //  private DcMotor dcmBackRightMotor;
-    private TouchSensor snsTouchTest;
-    private Sensor snsSense;
-    private DigitalChannel snsDigital;
-    private AnalogInput snsAnalog;
-
-    private static final double TICKSTOINCHES = (537.6 * 1) / (Math.PI * 3.77953);
-
-    private boolean bolGSToggle = false;
-    private boolean bolAWasPressed = false;
-    private boolean bolCarouselToggle = false;
-    private boolean bolXWasPressed = false;
-    private boolean bolRBumperWasPressed = false;
-    private boolean bolLBumperWasPressed = false;
-    private boolean bolBWasPressed = false;
-    private boolean bolYWasPressed = false;
-
-    private double dblCarouselSpeed;
-
-    private double dblCarouselSpeedToggle;
+    private DcMotor dcmFrontLeftMotor;
+    private DcMotor dcmFrontRightMotor;
+    private DcMotor dcmBackLeftMotor;
+    private DcMotor dcmBackRightMotor;
 
     public void init(){
-       // dcmCarouselMotor1 = hardwareMap.dcMotor.get("MotorFL");
-      //  dcmFrontRightMotor = hardwareMap.dcMotor.get("MotorFR");
-     //   dcmBackLeftMotor = hardwareMap.dcMotor.get("MotorBL");
-     //   dcmBackRightMotor = hardwareMap.dcMotor.get("MotorBR");
-
-        snsTouchTest = hardwareMap.touchSensor.get("TestTouch");
-        //snsSense = hardwareMap.get(Sensor.class, "Sense");
-        //nsDigital = hardwareMap.get(DigitalChannel.class, "digitalTouch");
-        snsAnalog = hardwareMap.analogInput.get("Analog");
+        dcmFrontLeftMotor = hardwareMap.dcMotor.get("MotorFL");
+        dcmFrontRightMotor = hardwareMap.dcMotor.get("MotorFR");
+        dcmBackLeftMotor = hardwareMap.dcMotor.get("MotorBL");
+        dcmBackRightMotor = hardwareMap.dcMotor.get("MotorBR");
     }
 
     public void loop(){
 
-
-
-        telemetry.addData("TestTouchValue", snsTouchTest.getValue());
-        telemetry.addData("TestTouch", snsTouchTest.isPressed());
-
-        //telemetry.addData("TestTouchValue", snsSense.getPower());
-        telemetry.addData("Analog", snsAnalog.getVoltage());
 
         telemetry.update();
     }
