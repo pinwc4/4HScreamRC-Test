eebf17284e83e95ae033befb8e9b88e5379084d2
OwenRex <owenrexshell@4hscream.org>
2023-01-12 19:39:42 -0600

    Reverted RoadRunner to base stats for 2nd Bot

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/DriveConstants.java
@@ -45,10 +45,10 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 0; //1.8898; // in
+    public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 13.3; // in
-    public static double WHEEL_BASE = 13.5; //in
+    public static double TRACK_WIDTH = 13.38; // in
+    public static double WHEEL_BASE = 11.35;//13.5; //in
 
     public static double getTrackWidth() {
         return TRACK_WIDTH;
@@ -64,9 +64,9 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.0165;
-    public static double kA = 0.0025;
-    public static double kStatic = 0.05654;
+    public static double kV = 1.0;//0.0165;
+    public static double kA = 0;//0.0025;
+    public static double kStatic =0;// 0.05654;
 
     /*
      * These values are used to generate the trajectories for you robot. To ensure proper operation,
@@ -96,10 +96,10 @@ public class DriveConstants {
      * You are free to raise this on your own if you would like. It is best determined through experimentation.
      
      */
-    public static double MAX_VEL = 45; //52.48291908330528
-    public static double MAX_ACCEL = 45; //52.48291908330528
-    public static double MAX_ANG_VEL = Math.toRadians(203.4541);
-    public static double MAX_ANG_ACCEL = Math.toRadians(203.4541);
+    public static double MAX_VEL = 52.48291908330528;//45 //52.48291908330528
+    public static double MAX_ACCEL = 52.48291908330528;//45 //52.48291908330528
+    public static double MAX_ANG_VEL = Math.toRadians(224.74213452914796);//(203.4541);
+    public static double MAX_ANG_ACCEL = Math.toRadians(224.74213452914796);//(203.4541);
 
 
     public static double encoderTicksToInches(double ticks) {
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
@@ -31,13 +31,13 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = 0.68371; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 12.7643; // in; distance between the left and right wheels
-    public static double FORWARD_OFFSET = -0.15; // in; offset of the lateral wheel
+    public static double LATERAL_DISTANCE = 13.76; // in; distance between the left and right wheels
+    public static double FORWARD_OFFSET = -0.11; // in; offset of the lateral wheel
 
     private Encoder leftEncoder, rightEncoder, frontEncoder;
 
-    public static double X_MULTIPLIER = 1.012;
-    public static double Y_MULTIPLIER = 1.0294;
+    public static double X_MULTIPLIER = 1;
+    public static double Y_MULTIPLIER = 1;
 
     public StandardTrackingWheelLocalizer(HardwareMap hardwareMap) {
         super(Arrays.asList(
