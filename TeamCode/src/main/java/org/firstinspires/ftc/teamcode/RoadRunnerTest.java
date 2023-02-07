0c62710b769c095c2dc6672d60522254e8aff170
OwenRex <owenrexshell@4hscream.org>
2023-02-06 21:21:03 -0600

    Added velocity restraint to Roadrunner code when sensing sleeve.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTest.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.MecanumVelocityConstraint;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.RoadRunner.drive.DriveConstants;
 import org.firstinspires.ftc.teamcode.RoadRunner.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.RoadRunner.trajectorysequence.TrajectorySequence;
 
@@ -16,6 +18,11 @@ public class RoadRunnerTest extends LinearOpMode {
     @Override
 
     public void runOpMode() throws InterruptedException{
+
+
+
+        MecanumVelocityConstraint slowMode = new MecanumVelocityConstraint(30, DriveConstants.getTrackWidth(), DriveConstants.getWheelBase());
+        MecanumVelocityConstraint normalMode = new MecanumVelocityConstraint(52, DriveConstants.getTrackWidth(), DriveConstants.getWheelBase());
 
         int intColorLevel = 0;
         int intConeStack1 = -95;
@@ -31,14 +38,19 @@ public class RoadRunnerTest extends LinearOpMode {
         drive.setPoseEstimate(startPose);
 
         TrajectorySequence traj1 = drive.trajectorySequenceBuilder(startPose)
+
+
                 .addTemporalMarker(1.5, () -> {
                     attachment.moveSlidesM();
                 })
                 .addTemporalMarker(1, () -> {
                     attachment.senseSleeve();
                 })
+
                 .splineToLinearHeading(new Pose2d(-34, -60, Math.toRadians(270)), Math.toRadians(0))
+                .setVelConstraint(slowMode)
                 .lineToLinearHeading(new Pose2d(-35, -9, Math.toRadians(270)))
+                .setVelConstraint(normalMode)
                 .splineToLinearHeading(new Pose2d(-27, -19, Math.toRadians(315)), Math.toRadians(-20))
 
                 .build();
