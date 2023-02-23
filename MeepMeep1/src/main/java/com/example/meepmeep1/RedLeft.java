11f5c2a980f0f55d8c49c13a0473f577c5d05bb5
ORaleigh12 <owenraleigh12@gmail.com>
2023-02-22 19:20:13 -0600

    Sped up Auto drop off

--- MeepMeep1/src/main/java/com/example/meepmeep1/RedLeft.java
+++ MeepMeep1/src/main/java/com/example/meepmeep1/RedLeft.java
@@ -5,11 +5,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-<<<<<<<< HEAD:MeepMeep1/src/main/java/com/example/meepmeep1/RedLeft.java
 public class RedLeft {
-========
-public class BlueLeft {
->>>>>>>> 9ca6c0f253f822cf0827f08756247ef759ce5c00:MeepMeep1/src/main/java/com/example/meepmeep1/BlueLeft.java
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -17,40 +13,48 @@ public class BlueLeft {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(39, -63, Math.toRadians(270)))
-                                .splineToLinearHeading(new Pose2d(35, -60, Math.toRadians(270)), Math.toRadians(46))
-                                .lineToLinearHeading(new Pose2d(35, -9, Math.toRadians(270)))
-                                .splineToLinearHeading(new Pose2d(28, -20, Math.toRadians(225)), Math.toRadians(46))
+                        drive.trajectorySequenceBuilder(new Pose2d(-39, -63, 270))
 
+
+                                //fast
+                                .splineToLinearHeading(new Pose2d(-35, -60, Math.toRadians(270)), Math.toRadians(0))
+                                //slow
                                 .setReversed(true)
-                                .lineToLinearHeading(new Pose2d(56, -12, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(60, -12, Math.toRadians(180)))
-                                .waitSeconds(0.25)
+                                .lineToLinearHeading(new Pose2d(-35, -27, Math.toRadians(270)))
+                                //fast
+                                .splineToSplineHeading(new Pose2d(-35, -9, Math.toRadians(290)), 310)
+                                .splineToLinearHeading(new Pose2d(-27, -19, Math.toRadians(315)), Math.toRadians(-20))
+
+
+                                /*
+                                .setReversed(true)
+                                .lineToLinearHeading(new Pose2d(-56, -11.75, Math.toRadians(360)))
+                                .lineToLinearHeading(new Pose2d(-60.8, -11.75, Math.toRadians(360)))
+
 
                                 .setReversed(false)
-                                .lineToLinearHeading(new Pose2d(56, -11, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(30, -21, Math.toRadians(225)))
+                                .lineToLinearHeading(new Pose2d(-56, -11, Math.toRadians(360)))
+                                .splineToLinearHeading(new Pose2d(-27, -21, Math.toRadians(325)), Math.toRadians(-20))
+
 
                                 .setReversed(true)
-                                .lineToLinearHeading(new Pose2d(56, -12.5, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(60, -12.5, Math.toRadians(180)))
-                                .waitSeconds(0.25)
+                                .lineToLinearHeading(new Pose2d(-56, -11, Math.toRadians(360)))
+                                .lineToLinearHeading(new Pose2d(-60.8, -11.3, Math.toRadians(360)))
 
-                                .setReversed(false)
-                                .lineToLinearHeading(new Pose2d(56, -11, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(30, -21, Math.toRadians(225)))
 
                                 .setReversed(true)
-                                .lineToLinearHeading(new Pose2d(56, -13, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(60 -13, Math.toRadians(180)))
-                                .waitSeconds(0.25)
+                                .lineToLinearHeading(new Pose2d(-56, -11, Math.toRadians(360)))
+                                .lineToLinearHeading(new Pose2d(-60.8, -10.9, Math.toRadians(360)))
 
-                                .setReversed(false)
-                                .lineToLinearHeading(new Pose2d(56, -11, Math.toRadians(180)))
-                                .lineToLinearHeading(new Pose2d(30, -21, Math.toRadians(225)))
 
-                                .build()
-                );
+                                .setReversed(true)
+                                .lineToLinearHeading(new Pose2d(-36, -12, Math.toRadians(270)))
+
+                                 */
+
+
+                                .build());
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
@@ -59,4 +63,3 @@ public class BlueLeft {
                 .start();
     }
 }
-
